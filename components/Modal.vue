569c25ecad0cb9b1858c8c58579b5980580c4064
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2024-02-14 21:15:32 +0500

    refactor: removed repeated code

--- components/Modal.vue
+++ components/Modal.vue
@@ -14,21 +14,13 @@ const cursorLabel = ref(null);
 const modalContainer = ref(null);
 const active = computed(() => props.modal.active);
 
-const animateElement = (element, properties) => gsap.to(element, properties);
-
 watch(active, (value) => {
-  const properties = {
-    x: "-50%",
-    y: "-50%",
-    duration: 0.5,
-  };
-
   CustomEase.create(
     "customEase",
     value ? "0.76, 0, 0.24, 1" : "0.32, 0, 0.67, 0"
   );
-  animateElement([modalContainer.value, cursorLabel.value], {
-    ...properties,
+  gsap.to([modalContainer.value, cursorLabel.value], {
+    duration: 0.5,
     scale: value ? 1 : 0,
     ease: "customEase",
   });
@@ -38,12 +30,6 @@ const onMouseMove = ref(() => {});
 onMounted(() => {
   const modal = modalContainer.value;
   const cursor = cursorLabel.value;
-
-  gsap.set([modal, cursor], {
-    scale: 0,
-    x: "-50%",
-    y: "-50%",
-  });
 
   const ease = "power3";
   const containerOptions = { duration: 0.8, ease };
@@ -73,30 +59,30 @@ onUnmounted(() => {
 <template>
   <div
     ref="modalContainer"
-    class="h-[350px] w-[400px] absolute bg-white overflow-hidden pointer-events-none flex items-center justify-center"
+    class="h-[350px] w-[400px] scale-0 -translate-x-1/2 -translate-y-1/2 absolute bg-white overflow-hidden pointer-events-none flex items-center justify-center"
   >
     <div
       :style="top"
       class="h-full w-full absolute transition-[top] duration-[0.5s] ease=[cubic-bezier(0.76,0,0.24,1)]"
     >
       <div
-        v-for="(project, index) in projects"
+        v-for="({ color, src }, index) in projects"
         :key="`modal_${index}`"
-        :style="{ backgroundColor: project.color }"
+        :style="{ backgroundColor: color }"
         class="h-full w-full flex items-center justify-center"
       >
         <NuxtImg
           class="h-auto w-auto"
           alt="image"
           width="300"
-          :src="`/images/${project.src}`"
+          :src="`/images/${src}`"
         />
       </div>
     </div>
   </div>
   <div
     ref="cursorLabel"
-    class="w-[80px] h-[80px] rounded-full bg-[#455CE9] text-white absolute z-[2] flex items-center justify-center text-sm font-light pointer-events-none"
+    class="w-[80px] h-[80px] scale-0 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#455CE9] text-white absolute z-[2] flex items-center justify-center text-sm font-light pointer-events-none"
   >
     View
   </div>
